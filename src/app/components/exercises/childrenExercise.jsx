a12bb3506138166ab9a8cbe258a79dafc13961b8
Igor <107798785+Yurtay@users.noreply.github.com>
2022-12-29 16:39:50 +0500

    Add files via upload

--- src/app/components/exercises/childrenExercise.jsx
+++ src/app/components/exercises/childrenExercise.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import CollapseWrapper from "../common/collapse";
+import PropTypes from "prop-types";
+
+const FormComponent = ({ children }) => {
+    return React.Children.map(children, (child, i) => {
+        const config = { index: i };
+
+        return React.cloneElement(child, config);
+    });
+};
+
 const ChildrenExercise = () => {
     return (
         <CollapseWrapper title="Упражнение">
@@ -10,16 +20,26 @@ const ChildrenExercise = () => {
                 <code>React.Children.map</code> так и{" "}
                 <code>React.Children.toArray</code>
             </p>
-
-            <Component />
-            <Component />
-            <Component />
+            <FormComponent>
+                <Component />
+                <Component />
+                <Component />
+            </FormComponent>
         </CollapseWrapper>
     );
 };
 
-const Component = () => {
-    return <div>Компонент списка</div>;
+FormComponent.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
 };
 
+const Component = ({ index }) => {
+    return <div>Компонент списка № {index}</div>;
+};
+Component.propTypes = {
+    index: PropTypes.number
+};
 export default ChildrenExercise;
